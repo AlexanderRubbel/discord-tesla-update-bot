90c2aae5fbf5949909cdaa583cfa7af8a6d1bf5e
AlexanderRubbel <Alexander.rubbel@web.de>
2026-04-30 14:59:28 +0200

    Change poll to version install status survey

--- utils/poll_builder.py
+++ utils/poll_builder.py
@@ -5,13 +5,11 @@ import discord
 
 def build_version_poll(version_id: str) -> discord.Poll:
     poll = discord.Poll(
-        question=f"Does FSD {version_id} work on your hardware?",
+        question=f"Have you installed version {version_id}?",
         duration=datetime.timedelta(days=7),
         multiple=False,
     )
-    poll.add_answer(text="HW3 working", emoji="\U0001f7e2")
-    poll.add_answer(text="HW4 working", emoji="\U0001f7e2")
-    poll.add_answer(text="Both working", emoji="\u2705")
-    poll.add_answer(text="HW3 not working", emoji="\U0001f534")
-    poll.add_answer(text="HW4 not working", emoji="\U0001f534")
+    poll.add_answer(text="Yes, installed", emoji="\u2705")
+    poll.add_answer(text="Not yet", emoji="\u23f3")
+    poll.add_answer(text="No, skipping this one", emoji="\u274c")
     return poll
